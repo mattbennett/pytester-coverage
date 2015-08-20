63dbeb19b36e2bd8890a0ce8c493a8b677fc54a2
Matt Bennett <matt@bennett.name>
2015-08-20 12:38:23 +0100

    use absolute path to rcfile rather than duplicating

--- test/conftest.py
+++ test/conftest.py
@@ -7,16 +7,14 @@ import pytest
 @pytest.yield_fixture
 def testdir(testdir):
 
-    os.environ['COVERAGE_PROCESS_START'] = ".coveragerc"
+    rc_path = os.path.join(
+        os.path.dirname(__file__), os.pardir, '.coveragerc'
+    )
+    os.environ['COVERAGE_PROCESS_START'] = os.path.normpath(rc_path)
 
     testdir.makepyfile(sitecustomize="""
         import coverage; coverage.process_startup()
     """)
-
-    fh = testdir.tmpdir.join(".coveragerc")
-    project_root = os.path.join(os.path.dirname(__file__), os.path.pardir)
-    with open(os.path.join(project_root, '.coveragerc')) as rc:
-        fh.write(rc.read())
 
     yield testdir
 
